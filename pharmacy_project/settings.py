42f92f41e7e86c9a3924323aa6e8de6f145d9071
hachokake <hachokake@users.noreply.github.com>
2026-03-19 16:03:20 +0100

    Add default SECRET_KEY for Railway build phase

--- pharmacy_project/settings.py
+++ pharmacy_project/settings.py
@@ -27,7 +27,7 @@ load_dotenv(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY') 
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-build-key-change-in-production') 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
